2fdbcfc0a6749b17051810201428dda8f96a8206
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2023-11-16 18:54:14 -0600

    Binary Search Done & Big O Notes Started

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,4 @@
-from colorama import Fore  # colors for tests
 from jovian.pythondsa import evaluate_test_cases
-
 
 """QUESTION 1: Alice has some cards with numbers written on them. She arranges the cards in decreasing
 order, and lays them out face down in a sequence on a table. She challenges Bob to pick out the card
@@ -21,8 +19,7 @@ containing a given number by turning over as few cards as possible. Write a func
 """ BRUTE FORCE SIMPLE SOLUTION
 1. if the target card is not within the cards list, return -1
 2. else, go through each element of the cards list and compare the value with the target
-3. if the value == the target value, return the current index"""
-
+3. if the value == the target value, return the current index
 
 def locate_card(cards, target):
     if target not in cards:
@@ -31,9 +28,62 @@ def locate_card(cards, target):
         for i in range(len(cards)):
             if cards[i] == target:
                 return i
+"""
+
+""" FINAL SOLUTION: Binary Search 
+1. Begin the algorithm at the middle of the cards list. (length of the list divided by 2)
+2. If the value at the middle == the target, return the index.
+3. If it is less than the target number search the first half of the list.
+4. If it is greater than the target number, search the second half of the list.
+5. If no more elements remain return -1
+"""
 
 
-# writing test with sample inputs and outputs ensures that the code behaves the way it should in all cases
+def binary_search(lo, hi, condition):
+    while lo <= hi:  # as long as we still have elements in the list, we run binary search
+        mid = (lo + hi) // 2  # define the middle index value as half of the list
+        result = condition(mid)  # call the "test_location" function to locate target value
+
+        if result == 'found':  # return value if it is found
+            return mid
+        elif result == 'left':  # shrink the cards list search scope to equal one less than the current middle value
+            hi = mid - 1
+        elif result == 'right':
+            lo = mid + 1  # shrink the cards list search scope to equal one more than the current middle value
+    return -1  # return -1 if the whole cards list is searched and the value is not found
+
+
+""" The "locate_card" function is used to find the middle index and pass it to the "test_location" function.
+If the "test_location" returns found, then the middle has been found, else we alter the list scope
+either to the left or the right and begin the search again. If the value isn't found by the time the "lo" bound 
+of the list is not less than the "hi" value, then return -1.
+Its important to note that by using this while-loop method, the original cards list is never actually modified, but
+the scope that is searched through is changed on each loop. Using a while loop is best when we cannot say for sure
+just how many iterations there will be before we find our result.
+"""
+
+
+def locate_card(cards, target):  # takes in the middle value, the cards list, and target location
+
+    def condition(mid):
+        if cards[mid] == target:  # if the middle number is the target...
+            if mid > 0 and cards[mid-1] == target:  # and its left neighbor is not out of bounds and is also the target.
+                return 'left'  # the value left of the found index also is the target value
+            else:
+                return 'found'  # if the value to the left is not also the target then we have found the target
+        elif cards[mid] < target:  # return left if the middle number is left of the target
+            return 'left'
+        else:
+            return 'right'  # return right if the middle number is left of the target
+    return binary_search(0, len(cards) - 1, condition)  # call binary search
+
+
+"""
+TIME COMPLEXITY: 0(log n)
+   with each iteration, the size of the data set gets halved 
+"""
+#  writing test with sample inputs and outputs ensures that the code behaves the way it should in all cases
+
 tests = [{  # target occurs in the middle
     'input': {
         "cards": [13, 11, 10, 7, 4, 3, 1, 0],
@@ -82,6 +132,5 @@ tests = [{  # target occurs in the middle
         'target': 6
     },
     'output': 2}]
-
 
 evaluate_test_cases(locate_card, tests)
