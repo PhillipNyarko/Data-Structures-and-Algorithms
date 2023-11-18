cd4ff99f00aff875725ec71126be2de395eda607
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2023-11-18 06:34:27 -0600

    Working on binary search method for rotated lists

--- Binary Search/rotated_lists.py
+++ Binary Search/rotated_lists.py
@@ -1,36 +1,67 @@
 from jovian.pythondsa import evaluate_test_cases
 
 
-def is_sorted(nums):  # use a for loop to check if the list is sorted
-    def rotate():
+def rotate(nums, rotations):
+    for i in range(rotations):
         last = nums[-1]
-        nums_copy = nums.copy()
+        rest = nums[:len(nums)-1]
         nums.clear()
         nums.append(last)
-        for j in range(len(nums_copy) - 1):
-            nums.append(nums_copy[j])
-        # print(nums)
-
-    for i in range(len(nums) - 1):
-        if nums[i] > nums[i + 1]:
-            rotate()
-            return False
-    return True
+        for j in range(len(rest)):
+            nums.append(rest[j])
+    return nums
 
 
 def count_rotations(nums):
-    rotations = - 1 if len(nums) > 1 else 0
-    while not is_sorted(nums):
-        rotations += 1
-    return rotations
+    pass
 
 
 # write test cases
 tests = [{
-    'input': {  # basic, normative input
-        'nums': [5, 6, 9, 0, 2, 3, 4]
+    'input': {  # length of 10 rotated 3 times
+        'nums': rotate([1, 2, 3, 4, 5, 6, 7, 8], 3)
     },
     'output': 3
+}, {
+    'input': {  # length of 8 rotated 5 times
+        'nums': rotate([1, 2, 3, 4, 5, 6, 7, 8], 5)
+    },
+    'output': 5
+}, {
+    'input': {  # rotated just once
+        'nums': rotate([1, 2, 3, 4, 5, 6, 7, 8], 1)
+    },
+    'output': 1
+}, {
+    'input': {  # rotated n-1 times, where n is the length of the list
+        'nums': rotate([1, 2, 3, 4, 5, 6, 7, 8], 7)
+    },
+    'output': 7
+}, {
+    'input': {  # rotated n times, where n is the length of the list
+        'nums': rotate([1, 2, 3, 4, 5, 6, 7, 8], 8)
+    },
+    'output': 0
+},  {
+    'input': {  # rotated n + 1 times, where n is the length of the list
+        'nums': rotate([i for i in range(100)], 101)
+    },
+    'output': 1
+}, {
+    'input': {  # not rotated at all
+        'nums': [1, 2, 3, 4, 5, 6]
+    },
+    'output': 0
+}, {
+    'input': {  # one element
+        'nums': [1]
+    },
+    'output': 0
+}, {
+    'input': {  # empty list
+        'nums': []
+    },
+    'output': -1
 }]
 
 evaluate_test_cases(count_rotations, tests)
