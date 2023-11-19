1f6eec73b62c44a69baaf86bdffb8d137fdf66da
Phillip Nyarko <phillip.k.nyarko@gmail.com>
2023-11-19 02:29:10 -0600

    count_rotations_binary completed comments added notes added tests passed

--- Binary Search/rotated_lists.py
+++ Binary Search/rotated_lists.py
@@ -1,19 +1,40 @@
 from jovian.pythondsa import evaluate_test_cases
 
 
-def rotate(nums, rotations):
-    for i in range(rotations):
-        last = nums[-1]
-        rest = nums[:len(nums)-1]
-        nums.clear()
-        nums.append(last)
-        for j in range(len(rest)):
+def rotate(nums, rotations):  # this function "rotates" a given list, and is only used to fill the test cases
+    for i in range(rotations):  # preform the rotation steps a specified number of times
+        last = nums[-1]  # take the last number of the list and store it temporarily
+        rest = nums[:len(nums)-1]  # take every element but the last element and store it temporarily
+        nums.clear()  # clear the number list
+        nums.append(last)  # put the last element first
+        for j in range(len(rest)):  # use a for loop to append the rest of the elements after the original last element
             nums.append(rest[j])
-    return nums
+    return nums  # return the new number array
 
 
-def count_rotations(nums):
-    pass
+"""
+be wary of preforming operations/modifications on a list without making a copy and using the copy 
+as it can have unintended consequences
+"""
+
+
+def count_rotations_binary(nums):  # essentially, return the index of the smallest number in the list
+    low = 0  # this is a "pointer" of sorts that points to the lowest index that will be looked at in the binary search
+    high = len(nums) - 1  # this is also a "pointer" that points to the highest part that will be searched
+
+    # to look through the whole list, we must keep looking until the high value is less than the low
+    # This allows the function to run even when the list has one element, and it stops after the list is empty
+
+    while low <= high:
+        middle_index = (low + high) // 2  # this method because it accounts for a change in the "low" or "high" value
+        middle_number = nums[middle_index]  # get the value of the number at the middle index.
+        if middle_number < nums[middle_index - 1] or len(nums) == 1:  # middle number < the number to the left of it?
+            return middle_index  # return that index, because the number at that index is the smallest in the list
+        elif middle_number < nums[-1]:  # if the middle number is less than the last number in the list
+            high = middle_index - 1  # move the high index pointer to the value of the middle index -1
+        else:
+            low = middle_index + 1  # move the low index pointer to the value of the middle index +1
+    return -1  # return negative 1 if the value is not in the list.
 
 
 # write test cases
@@ -64,4 +85,4 @@ tests = [{
     'output': -1
 }]
 
-evaluate_test_cases(count_rotations, tests)
+evaluate_test_cases(count_rotations_binary, tests)
