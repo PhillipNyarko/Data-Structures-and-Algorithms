aa531d9131d258df952d13373d973aa17087e152
Phillip Nyarko <Phillip.k.Nyarko@gmail.com>
2024-02-09 01:19:52 -0600

    working on binary trees

--- Lessons/Binary Trees/binary_trees.py
+++ Lessons/Binary Trees/binary_trees.py
@@ -13,7 +13,6 @@ class User:
 
 
 class UserDatabase:
-
     def __init__(self):
         self.users = []  # define a list to store all the users in the database
 
@@ -47,19 +46,33 @@ elda = User("eldaNotEldin", "Elda Minuet", "MinuetaElda@outlook.com")
 princess = User("princessesAndPeaches", "Princess Imbatu", "ImbatuPrincess@gmail.com")
 olaf = User("olafIsUnderTheWeather", "Olaf Snow", "SnowOlaf@aol.com")
 
-Users = [bob, tim, mary, cameron, elda, princess, olaf]
-
 database = UserDatabase()
 
-database.insert(bob)
-database.insert(tim)
-database.insert(mary)
+class TreeNode:
+    def __init__(self, key) :
+        self.key = key  # every node gets a key as an identifier...
+        self.left = None  # and a left
+        self.right = None # and right node
 
-user_to_be_located = database.find('maryLovesFlowers')
-print(user_to_be_located)
 
-print(database.list_all())
+def parse_tuple(data):
+    # print(data)
+    if isinstance(data, tuple) and len(data) == 3:  # if the passed in data is a tuple and the tuple is a length of 3...
+        node = TreeNode(data[1])  # set the root node to equal the second value in the passed in tuple(should be root)
+        node.left = parse_tuple(data[0])  # set the root node's left value to the first item in the passed in tuple
+        node.right = parse_tuple(data[2])  # set the root node's right value to the last item in the passed in tuple
 
-database.insert(cameron)
+    elif data is None: # base case reached when there are no more nodes to look through
+        node = None
+    else:
+        #  if we havent reached the base case, set the node to equal a new node with the subset of data passed in.
+        node = TreeNode(data)
 
-print(database.list_all())
+
+tree = parse_tuple(((1, 3, None), 2, ((None,3, 4), 5, (6, 7, 8))))
+
+print(tree)  # should be 2
+print(tree.left.key, tree.right.key)  # should be 3, 5
+print(tree.left.left.key, tree.left.right.key, tree.right.left.key, tree.right.right.key)  # should be 1, None, 3, 7
+print(tree.right.left.right.key, tree.right.right.left.key, tree.right.right.right.key)  # should be 4, 6, 8
+
